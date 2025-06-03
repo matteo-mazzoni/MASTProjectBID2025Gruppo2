06541f7fd93a8ec8400b397bc0a163942476655b
Romina Trazzi <romina.trazzi@edu-its.it>
2025-06-03 05:14:23 +0200

    Added project scaffolding

---
+++ readup/src/main/java/com/mast/readup/ReadupApplication.java
@@ -0,0 +1,13 @@
+package com.mast.readup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadupApplication.class, args);
+	}
+
+}
---
+++ readup/src/test/java/com/mast/readup/ReadupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mast.readup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
