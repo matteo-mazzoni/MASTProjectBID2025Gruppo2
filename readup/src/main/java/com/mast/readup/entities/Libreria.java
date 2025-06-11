32fa0ac80e0eed10e161ac38dceeeaf0b2507749
Romina Trazzi <romina.trazzi@edu-its.it>
2025-06-11 19:53:49 +0200

    Fix minor bugs in class LibreriaKey

--- readup/src/main/java/com/mast/readup/entities/Libreria.java
+++ readup/src/main/java/com/mast/readup/entities/Libreria.java
@@ -1,4 +1,6 @@
 package com.mast.readup.entities;
+import java.util.Objects;
+
 import jakarta.persistence.*;
 
 // These annotations indicates that this class is an entity and will be mapped to a database table called "libreria".
@@ -17,12 +19,12 @@ public class Libreria {
     // Navigation properties: these properties are used to establish the relationship between Libreria entity and Libro and Utente entities.
     @MapsId("idLibro")
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_libro", insertable = false, updatable = false)
+    @JoinColumn(name = "id_libro")
     private Libro libro;
 
     @MapsId("idUtente")
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id_utente", insertable = false, updatable = false)
+    @JoinColumn(name = "id_utente")
     private Utente utente;
 
     // Basic constructor 
@@ -50,5 +52,18 @@ public class Libreria {
     public void setStatoPrestito(String statoPrestito) 
     { 
         this.statoPrestito = statoPrestito; 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Libreria)) return false;
+        Libreria that = (Libreria) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
