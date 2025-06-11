f4da8b04c4d395c5c3f9b80a57e4b62d30f4c288
Romina Trazzi <romina.trazzi@edu-its.it>
2025-06-11 21:42:40 +0200

    Refactoring Libreria and LibreriaKey classes using LomboK

--- readup/src/main/java/com/mast/readup/entities/BooklistContiene.java
+++ readup/src/main/java/com/mast/readup/entities/BooklistContiene.java
@@ -1,4 +1,5 @@
 package com.mast.readup.entities;
+
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -6,86 +7,42 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
-import java.util.Objects;
 
-// These annotations indicates that this class is an entity and will be mapped to a database table called "booklist_contiene".
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Entity representing the "booklist_contiene" table.
+ * Models a many-to-many relationship between Booklist and Libro,
+ * associated with a specific Utente, using a composite key.
+ */
 @Entity
 @Table(name = "booklist_contiene")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BooklistContiene {
 
-    // Link to the composite key class BooklistContieneKey
+    // Composite primary key (book + booklist + user)
     @EmbeddedId
     private BooklistContieneKey id;
 
-    // Navigation properties: these properties are used to establish the relationship between BooklistContiene entity and Libro, Booklist, and Utente entities.
+    // Associated book (part of composite key)
     @MapsId("idLibro")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_libro")
     private Libro libro;
 
+    // Associated booklist (part of composite key)
     @MapsId("idBooklist")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_booklist")
     private Booklist booklist;
 
+    // Associated user (part of composite key)
     @MapsId("idUtente")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "id_utente")
     private Utente utente;
-
-    // Basic constructor
-    public BooklistContiene() {}
-
-    // Constructor with parameters
-    public BooklistContiene(BooklistContieneKey id) {
-        this.id = id;
-    }
-
-    // Getter / Setter
-    public BooklistContieneKey getId() {
-        return id;
-    }
-
-    public void setId(BooklistContieneKey id) {
-        this.id = id;
-    }
-
-    public Libro getLibro() {
-        return libro;
-    }
-
-    public void setLibro(Libro libro) {
-        this.libro = libro;
-    }
-
-    public Booklist getBooklist() {
-        return booklist;
-    }
-
-    public void setBooklist(Booklist booklist) {
-        this.booklist = booklist;
-    }
-
-    public Utente getUtente() {
-        return utente;
-    }
-
-    public void setUtente(Utente utente) {
-        this.utente = utente;
-    }
-
-    // Overrides equals(Object) to compare BooklistContiene instances by id for logical equality
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof BooklistContiene)) return false;
-        BooklistContiene that = (BooklistContiene) o;
-        return Objects.equals(id, that.id);
-    }
-
-    // Overrides hashCode() to generate a consistent hash based on id, ensuring equal entities yield the same hash
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
