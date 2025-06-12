9fed030577ea8ba573bfc95309c7258d34c75597
Valentina_PC <valentina.slaviero@edu-its.it>
2025-06-12 16:57:15 +0200

    Change class JsonStringListConverter into interface JsonStringListConverter and implemented in Sfida and Booklist

--- readup/src/main/java/com/mast/readup/entities/JsonStringListConverter.java
+++ readup/src/main/java/com/mast/readup/entities/JsonStringListConverter.java
@@ -1,31 +1,10 @@
 package com.mast.readup.entities;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.persistence.AttributeConverter;
-import jakarta.persistence.Converter;
 import java.util.List;
 
-@Converter
-public class JsonStringListConverter implements AttributeConverter<List<String>, String> {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+public interface JsonStringListConverter {
 
-    @Override
-    public String convertToDatabaseColumn(List<String> attribute) {
-        try {
-            return objectMapper.writeValueAsString(attribute);
-        } catch (Exception e) {
-            throw new RuntimeException("Error converting list to JSON", e);
-        }
-    }
+    String convertToDatabaseColumn(List<String> attribute);
 
-    @Override
-    public List<String> convertToEntityAttribute(String dbData) {
-        try {
-            return objectMapper.readValue(dbData, new TypeReference<List<String>>() {
-            });
-        } catch (Exception e) {
-            throw new RuntimeException("Error converting JSON to list", e);
-        }
-    }
+    List<String> convertToEntityAttribute(String dbData);
 }
