14d031d2ebcc19792863ebff403f95e185f2421b
Matteo Mazzoni <matteo.mazzoni@edu-its.it>
2025-06-12 11:56:29 +0200

    minor fix

--- readup/src/main/java/com/mast/readup/repos/BooklistRepos.java
+++ readup/src/main/java/com/mast/readup/repos/BooklistRepos.java
@@ -4,8 +4,6 @@ import com.mast.readup.entities.Booklist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-import java.util.List;
 
 @Repository
 public interface BooklistRepos extends JpaRepository<Booklist, Long> {
--- readup/src/main/java/com/mast/readup/repos/SfidaRepos.java
+++ readup/src/main/java/com/mast/readup/repos/SfidaRepos.java
@@ -4,8 +4,6 @@ import com.mast.readup.entities.Sfida;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-import java.util.List;
 
 @Repository
 public interface SfidaRepos extends JpaRepository<Sfida, Long> {
