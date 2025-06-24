b7422a063bc95d2495665a4d99770116f45c23f4
Romina Trazzi <romina.trazzi@edu-its.it>
2025-06-24 06:17:12 +0200

    Implemented logout (database field loggedIn is working

--- readup/src/main/java/com/mast/readup/controller/ReadUpMVC.java
+++ readup/src/main/java/com/mast/readup/controller/ReadUpMVC.java
@@ -7,18 +7,37 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mast.readup.entities.Utente;
+import com.mast.readup.services.BooklistService;
+import com.mast.readup.services.SfidaService;
 import com.mast.readup.services.UtenteService;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 
 @Controller
 public class ReadUpMVC {
 
-    // Home + Register
+    /* SERVICES INJECTION */
+
+    // Utente Service injection
+    @Autowired
+    private UtenteService utenteService;
+    
+    // Booklist Service injection
+    @Autowired
+    private BooklistService booklistService;
+
+    // Sfida Service injection
+    @Autowired
+    private SfidaService sfidaService;
+
+    
+    /* CONTROLLERS */
+    
+    // Home 
     @GetMapping("/")
     public String home(Model model) {
         if (!model.containsAttribute("Utente")) {
@@ -27,15 +46,10 @@ public class ReadUpMVC {
         return "index";
     }
 
-
-    // Service injection
-    @Autowired
-    private UtenteService utenteService;
-
     // New user registration
     @PostMapping("/register")
-    public String processRegister(@Valid @ModelAttribute("Utente") Utente utente,
-    BindingResult result, Model model, RedirectAttributes redirectAttributes) {
+    public String processRegister(@Valid @ModelAttribute("Utente") Utente utente, BindingResult result,
+    Model model, RedirectAttributes redirectAttributes, HttpSession session) {
     
         // Form validation server-side for duplicate nickname and email
         if (!result.hasFieldErrors("nickname") && utenteService.nicknameEsistente(utente.getNickname())) {
@@ -61,35 +75,44 @@ public class ReadUpMVC {
             return "index";
         }
        
-        // If there are no errors, register the user into the database and set the login property to "true"
+        // If there are no errors, register the user into the database and set the login property to "true" in database
         utente.setLoggedIn(true);
-        utenteService.aggiungiUtente(utente);
 
-        // Messaggio di successo (mostro solo se presente)                           
-        redirectAttributes.addFlashAttribute("successMessage",
-            "Benvenuto “" + utente.getNickname() + "”! Registrazione avvenuta con successo."
-        );
+        // All the user data are stored in an object Utente-type called saved. The new user is added to the database
+        Utente saved = utenteService.aggiungiUtente(utente);
 
-        return "redirect:/";
+        /* Save the user data in the session.
+        This allows to have the exact primary key on later requests— it will be known which record to update, as set loggedIn to false on logout. */
+        session.setAttribute("currentUser", saved);
+
+        // Success message (linked to the changed view in HTML)                      
+        redirectAttributes.addFlashAttribute("successMessage", "Benvenuto/a " + saved.getNickname() + "!");
+        
+        // Redirect to the homepage
+        return "redirect:/";    
     }
 
     // User logout 
-   @PostMapping("/logout")
-    public String logout(SessionStatus sessionStatus, RedirectAttributes ra, @ModelAttribute("Utente") Utente utente) {
+    @PostMapping("/logout")
+    public String logout(HttpSession session) {
         
-        utente.setLoggedIn(false);
+        // select the user from the session
+        Utente current = (Utente) session.getAttribute("currentUser");
         
-        sessionStatus.setComplete();
-     
-        ra.addFlashAttribute("successMessage", "Logout avvenuto con successo.");
+        // if the user is logged in
+        if (current != null) {
+
+            // update the property loggedIn in the database to false
+            utenteService.cambiaStatusLogin(current.getIdUtente(), false);
+
+            // remove all attributes from the session (including the current user)
+            session.invalidate();
+        }
+    
+        // Redirect to the homepage
         return "redirect:/";
     }
     
-
-
-
-
-
     // Profilo
     @GetMapping("/profilo.html")
     public String profilo(Model model) {
