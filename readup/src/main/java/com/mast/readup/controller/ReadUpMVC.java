fde4da684693d8f50d70a508ceb79eb64a2c831f
Valentina_PC <valentina.slaviero@edu-its.it>
2025-06-26 15:41:21 +0200

    merge with changes

--- readup/src/main/java/com/mast/readup/controller/ReadUpMVC.java
+++ readup/src/main/java/com/mast/readup/controller/ReadUpMVC.java
@@ -3,18 +3,12 @@ package com.mast.readup.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mast.readup.entities.Sfida;
@@ -155,10 +149,10 @@ public class ReadUpMVC {
     public String qa() {
         return "qa";
     }
-}
-
 
     private Long getCurrentUserId(HttpSession session) {
         Utente currentUser = (Utente) session.getAttribute("currentUser");
         return (currentUser != null) ? currentUser.getIdUtente() : null;
     }
+
+}
