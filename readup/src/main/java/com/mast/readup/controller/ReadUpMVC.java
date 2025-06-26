a9f1761e499617d45719e31962bade59b25ba6c2
Matteo Mazzoni <matteo.mazzoni@edu-its.it>
2025-06-26 16:54:00 +0200

    added controllers for the pfp change.

--- readup/src/main/java/com/mast/readup/controller/ReadUpMVC.java
+++ readup/src/main/java/com/mast/readup/controller/ReadUpMVC.java
@@ -1,14 +1,20 @@
 package com.mast.readup.controller;
 
+import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mast.readup.entities.Sfida;
@@ -37,10 +43,10 @@ public class ReadUpMVC {
     @Autowired
     private SfidaService sfidaService;
 
-    
+
     /* CONTROLLERS */
-    
-    // Home 
+
+    // Home
     @GetMapping("/")
     public String home(Model model) {
         if (!model.containsAttribute("Utente")) {
@@ -53,19 +59,19 @@ public class ReadUpMVC {
     @PostMapping("/register")
     public String processRegister(@Valid @ModelAttribute("Utente") Utente utente, BindingResult result,
     Model model, RedirectAttributes redirectAttributes, HttpSession session) {
-    
+
         // Form validation server-side for duplicate nickname and email
         if (!result.hasFieldErrors("nickname") && utenteService.nicknameEsistente(utente.getNickname())) {
-              
+
             result.rejectValue(
                 "nickname",
                 "error.nickname.duplicate",
                 "ATTENZIONE: Questo username è già in uso! RIPROVA."
             );
         }
-        
+
         if (!result.hasFieldErrors("email") && utenteService.emailEsistente(utente.getEmail())) {
-             
+
             result.rejectValue(
                 "email",
                 "error.email.duplicate",
@@ -77,7 +83,7 @@ public class ReadUpMVC {
         if (result.hasErrors()) {
             return "index";
         }
-       
+
         // If there are no errors, register the user into the database and set the login property to "true" in database
         utente.setLoggedIn(true);
 
@@ -88,11 +94,11 @@ public class ReadUpMVC {
         This allows to have the exact primary key on later requests— it will be known which record to update, as set loggedIn to false on logout. */
         session.setAttribute("currentUser", saved);
 
-        // Success message (linked to the changed view in HTML)                      
+        // Success message (linked to the changed view in HTML)
         redirectAttributes.addFlashAttribute("successMessage", "Benvenuto/a " + saved.getNickname() + "!");
-        
+
         // Redirect to the homepage
-        return "redirect:/";    
+        return "redirect:/";
     }
 
     // User logout
@@ -122,6 +128,25 @@ public class ReadUpMVC {
         return "profilo";
     }
 
+        @PostMapping("/uploadprofileimage")
+    public String handleImageUpload(@RequestParam("image") MultipartFile file, Principal principal) {
+        if (principal == null || principal.getName() == null) {
+            return "redirect:/login?error=not_authenticated";
+        }
+        utenteService.saveProfileImage(principal.getName(), file);
+        return "redirect:/profile";
+    }
+
+    @GetMapping("/newprofileimage/{id}")
+    public ResponseEntity<byte[]> getImage(@PathVariable("id") Long idUtente) {
+        byte[] image = utenteService.getProfileImage(idUtente);
+        if (image != null) {
+            return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
+        } else {
+            return ResponseEntity.notFound().build(); // image not found
+        }
+    }
+
     // Le mie Booklist
     @GetMapping("/booklist.html")
     public String booklist(Model model) {
@@ -141,7 +166,24 @@ public class ReadUpMVC {
         model.addAttribute("sfide", sfide);
         model.addAttribute("currentUserId", getCurrentUserId(session));
         model.addAttribute("sfida", new Sfida()); //creates an empty Sfida object for the form
-        return "listaSfide";
+        return "sfide";
+    }
+
+    //salva sfida
+    @PostMapping("/salvasfida")
+    public String salvaSfida(@ModelAttribute("sfida") Sfida sfida,
+                            @RequestParam("idCreatoreForm") Long idCreatore,
+                            RedirectAttributes redirectAttributes) {
+        try {
+            sfidaService.aggiungiSfida(sfida.getNomeSfida(), sfida.getDescrizioneSfida(),
+                                    sfida.getDataInizio(), sfida.getDataFine(), idCreatore);
+            redirectAttributes.addFlashAttribute("successMessage", "Sfida creata con successo!");
+            return "redirect:/sfide"; // Torna alla lista aggiornata
+        } catch (IllegalArgumentException e) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Errore nella creazione della sfida: " + e.getMessage());
+            // Se c'è un errore, l'utente verrà reindirizzato alla pagina della lista.
+            return "redirect:/sfide"; // Reindirizza alla pagina della lista, mostrando l'errore
+        }
     }
 
     // Question and answers
@@ -149,10 +191,9 @@ public class ReadUpMVC {
     public String qa() {
         return "qa";
     }
-}
-
 
     private Long getCurrentUserId(HttpSession session) {
         Utente currentUser = (Utente) session.getAttribute("currentUser");
         return (currentUser != null) ? currentUser.getIdUtente() : null;
     }
+}
