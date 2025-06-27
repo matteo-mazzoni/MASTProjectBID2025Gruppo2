efa389338db4c17b8b7fe1dfd844d578fec45bc9
Matteo Mazzoni <matteo.mazzoni@edu-its.it>
2025-06-27 10:47:16 +0200

    fixed stuff

--- readup/src/main/java/com/mast/readup/entities/Libro.java
+++ readup/src/main/java/com/mast/readup/entities/Libro.java
@@ -76,6 +76,14 @@ public class Libro {
     // One-to-many relationship with the Contiene entity (another bridge table)
     @OneToMany(mappedBy = "libro", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<BooklistContiene> contenuti = new HashSet<>();
+
+    public String getCoverUrl() {
+        if (isbn == null || isbn.isBlank()) {
+            return "/img/outdoor-reading-group-stockcake.jpg";
+        }
+        // “-M” per dimensione Medium; puoi cambiare in S o L se preferisci
+        return "https://covers.openlibrary.org/b/isbn/" + isbn + "-M.jpg";
+    }
 }
 
 
